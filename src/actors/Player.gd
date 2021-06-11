c6a2a8b6f51b9bae3e013765edbc68af30395232
Jed Thompson <jedster1111@hotmail.co.uk>
2021-06-11 22:46:44 +0100

    Make dash fixed distance

--- src/actors/Player.gd
+++ src/actors/Player.gd
@@ -1,14 +1,13 @@
 extends KinematicBody2D
 
-
+export var max_dash_distance = 200
 export var speed = 1000
 var velocity = Vector2(0,0)
 var direction = Vector2(0,0)
 var start_pos = position
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	move_player()
-
 
 func move_player():
 	if Input.is_action_pressed("dash"):
@@ -16,6 +15,10 @@ func move_player():
 		start_pos = position
 		direction = (mouse_pos - position).normalized()
 		velocity =  move_and_slide(speed * direction)
+
+	var dash_distance = (position - start_pos).length()
+	if dash_distance > max_dash_distance:
+		direction = Vector2()
+		velocity = Vector2()
+	
 	velocity =  move_and_slide(velocity)
-
-
