5cf2c3934bbe761da52da4187b150d4b4ac03c3d
Jed Thompson <jedster1111@hotmail.co.uk>
2021-06-12 16:40:02 +0100

    Add in game gui & use a top level Game node

--- src/ui/StartScreen.gd
+++ src/ui/StartScreen.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 
 func _on_StartGame_pressed():
 	print("start")
-	var _success = get_tree().change_scene("res://src/levels/level-1.tscn")
+	var _success = get_tree().change_scene("res://src/Game.tscn")
 
 func _on_Exit_pressed():
 	get_tree().quit()
