ee28c495bddcf5603a1edc97688cb1a322cb7e43
[Justin Soh] <ys00715@surrey.ac.uk>
2021-06-13 16:33:10 +0100

    Added gameplay song

--- src/Game.gd
+++ src/Game.gd
@@ -7,7 +7,7 @@ var ninja_grade = "S+"
 var end_score = 0
 
 onready var inGameUi = get_node("InGameUI")
-
+onready var themeTune = get_node("ThemeTune")
 
 var current_level = 0
 var levels = ["tutorialLevel", "birdIntro", "level-1", "Justin_test_scene", "JumpingBuilding"]
@@ -17,6 +17,7 @@ func _ready():
 	_load_level()
 	calculate_scores()
 	set_child_values()
+	themeTune.play()
 
 func _on_next_level_selected():
 	current_level += 1
