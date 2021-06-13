1511a5d0cae37f761925a9333e5c839dbdf3413c
[Justin Soh] <ys00715@surrey.ac.uk>
2021-06-13 15:58:49 +0100

    Added Jumping Building level

--- src/Game.gd
+++ src/Game.gd
@@ -10,7 +10,7 @@ onready var inGameUi = get_node("InGameUI")
 
 
 var current_level = 0
-var levels = ["level-1", "Justin_test_scene", "tutorialLevel", "birdIntro"]
+var levels = ["level-1", "Justin_test_scene", "tutorialLevel", "birdIntro", "JumpingBuilding"]
 var level
 
 func _ready():
