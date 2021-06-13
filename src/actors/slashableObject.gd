084ed7c1ab05bd825676bd91d8160a31930e4288
Jed Thompson <jedster1111@hotmail.co.uk>
2021-06-13 01:00:07 +0100

    Add enemy death grunt

--- src/actors/slashableObject.gd
+++ src/actors/slashableObject.gd
@@ -3,7 +3,8 @@ class_name SlashableBody
 
 signal points_earned(points)
 
-var BloodSplatter = preload ("res://src/actors/BloodSplatter.tscn")
+var DestructionScene = load("res://src/actors/BloodSplatter.tscn")
+var audio_stream = load("res://assets/audio/Death_2.wav")
 
 var points = 100
 var max_lives  = 1
@@ -16,14 +17,19 @@ func hit(hit_direction: Vector2):
 	emit_signal("points_earned", points)
 	max_lives -= 1
 
-	var blood = BloodSplatter.instance()
-	blood.position += hit_direction.normalized() * 40
-	blood.rotation = hit_direction.angle()
-	blood.scale *= 1.5
-	add_child(blood)
-	blood.stop()
-	blood.play("default")
-	yield(blood, "animation_finished")
+	var destruction_instance = DestructionScene.instance()
+	destruction_instance.position += hit_direction.normalized() * 40
+	destruction_instance.rotation = hit_direction.angle()
+	destruction_instance.scale *= 1.5
+
+	add_child(destruction_instance)
+
+	destruction_instance.play("default")
+	var audio_player = AudioStreamPlayer2D.new()
+	audio_player.stream = audio_stream
+	add_child(audio_player)
+	audio_player.play()
+	yield(destruction_instance, "animation_finished")
 
 	if max_lives <= 0:
 		die()
